f6def38c32b49a074496a6b3d6dfbc6a8a99c9fd
fxanomaly@gmail.com <fxanomaly@gmail.com>
2019-01-20 14:48:17 +0000

    feature1-commit-1 localStorage implemented

---
+++ rebase1/src/localStorage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class localStorage extends Component {
+  static propTypes = {
+    prop: PropTypes
+  }
+
+  render() {
+    return (
+      <div>
+        local storage added !!
+      </div>
+    )
+  }
+}
